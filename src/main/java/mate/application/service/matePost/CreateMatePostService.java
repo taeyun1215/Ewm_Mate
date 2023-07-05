dcd3a0fb7380c5bac5c2798b8b629591b7e1ec2e
taeyun1215 <taeyun1215@naver.com>
2023-07-05 16:03:53 +0900

    fix : spring cloud id

--- src/main/java/mate/application/service/matePost/CreateMatePostService.java
+++ src/main/java/mate/application/service/matePost/CreateMatePostService.java
@@ -7,6 +7,8 @@ import mate.application.port.in.usecase.CreateMatePostUseCase;
 import mate.application.port.out.matePost.SaveMatePostPort;
 import mate.domain.MatePost;
 import mate.global.annotation.UseCase;
+import mate.global.kafka.KafkaProducer;
+import org.apache.kafka.common.internals.Topic;
 
 import javax.transaction.Transactional;
 
@@ -17,11 +19,13 @@ import javax.transaction.Transactional;
 public class CreateMatePostService implements CreateMatePostUseCase {
 
     private final SaveMatePostPort saveMatePostPort;
+    private final KafkaProducer kafkaProducer;
 
     @Override
     public void createMatePost(CreateMatePostRequest createMatePostRequest, Long userId) {
         MatePost matepost = createMatePostRequest.toEntity(userId);
         saveMatePostPort.saveMatePost(matepost);
+        kafkaProducer.send("user-topic", createMatePostRequest);
     }
 
 }
